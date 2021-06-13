0118aadb0e31249f1266d7156a017b1f49e8396b
Adam <adam@bonsu.io>
2021-06-13 22:17:53 +0100

    randomise question order unless specified otherwise on the command line

--- README.md
+++ README.md
@@ -8,4 +8,20 @@ Install the gem:
 `gem install one44-cli`
 
 ## How to use
+
+### Randomly order test questions (default)
+Test questions are randomly ordered. Make the behaviour explicit by specifying one of the options below:
+
 $`one44-cli`
+
+$`one44-cli -r`
+
+$`one44-cli --random-sort-questions`
+
+$`one44-cli --sort-questions random`
+
+
+### Do not randomly order test questions
+Test questions will be ordered as listed if the `--not-random-sort-questions` is specified:
+
+$`one44-cli --not-random-sort-questions`
--- lib/one44-cli/optparse.rb
+++ lib/one44-cli/optparse.rb
@@ -5,23 +5,25 @@ module One44
     module Optparse
       require 'optparse'
 
-      options = {}
+      $one44_cli_options = {}
       option_parser = OptionParser.new do |opts|
-        # Create a switch
         opts.on('-r', '--random-sort-questions') do
-          options[:random_sort_questions] = true
+          $one44_cli_options[:random_sort_questions] = true
         end
 
         opts.on('--not-random-sort-questions') do
-          options[:random_sort_questions] = false
+          $one44_cli_options[:random_sort_questions] = false
         end
 
-        opts.on('-s ORDER', '--sort-questions=ORDER') do |order|
-          options[:question_order] = order
+        opts.on('-s ORDER', '--sort-questions=ORDER', /^random$/i) do |order|
+          $one44_cli_options[:question_order] = order.downcase.to_sym
         end
       end
       option_parser.parse!
-      puts options.inspect
+
+      unless $one44_cli_options&.dig(:question_order)
+        $one44_cli_options[:question_order] = :random unless $one44_cli_options&.dig(:random_sort_questions) == false
+      end
     end
   end
 end
