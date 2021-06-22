8cf41c536a5cf3dabed60137b528ded3fed599a1
Adam <adam@bonsu.io>
2021-06-22 16:37:11 +0100

    test completes faster after reducing configured exit timeout

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require 'aruba/cucumber'
+
+Aruba.configure do |config|
+  config.exit_timeout                          = 0.5
+  config.io_wait_timeout                       = 2
+  config.activate_announcer_on_command_failure = [:stderr, :stdout, :command]
+end
