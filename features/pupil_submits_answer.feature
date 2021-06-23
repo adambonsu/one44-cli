6ee2bd3e208f6585d5ed81b2fd23537bc29e97f4
Adam <adam@bonsu.io>
2021-06-23 18:30:37 +0100

    features/pupil_submits_answer.feature is green

--- features/pupil_submits_answer.feature
+++ features/pupil_submits_answer.feature
@@ -3,15 +3,18 @@ Feature: Pupil Submits Answer
   Pupil is presented with mental math questions
   Have 5 minutes to provide all answers
 
-  Scenario Outline: Pupil Submits Answer
-    Given the Question is <question>
-    When I answer <answer>
-    Then my answer is marked as <mark>
 
-    Examples: Correct Answer
-      | question | answer | mark    |
-      | 1x8      | 8      | CORRECT |
+  Scenario: Pupil Submits Correct Answer - Mark is CORRECT
 
-    Examples: Incorrect Answer
-      | question | answer | mark      |
-      | 1x8      | 1      | INCORRECT |
+    Given I provide a correct Answer to a Test Question:
+      | question | answer |
+      | 1x5      | 5      |
+    Then the Mark for my Answer will be CORRECT
+
+
+  Scenario: Pupil Submits Incorrect Answer - Mark is INCORRECT
+
+    Given I provide an incorrect Answer to a Test Question:
+      | question | answer |
+      | 1x8      | 9      |
+    Then the Mark for my Answer will be INCORRECT
--- features/step_definitions/provide_answers.rb
+++ features/step_definitions/provide_answers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def provide_test(answers)
-  type(unescape_text(answers.join("\n")))
+  type(unescape_text(answers))
 end
 
 Given('I provide the following Answers for these {string} Test Questions:') do |test_name, table|
@@ -14,10 +14,29 @@ Given('I provide the following Answers for these {string} Test Questions:') do |
   @test_name = test_name
   create_test_file(@test_name, @questions.join("\n"))
 
-  start_test("one44-cli -t '#{test_name}'")
-  provide_test(@answers)
+  @one44_command = "one44-cli -t '#{test_name}'"
+  start_test(@one44_command)
+  provide_test(@answers.join("\n"))
+end
+
+When(/I provide a(?:n in| )correct Answer to a Test Question:/) do |table|
+  questions_answers = table.hashes
+
+  @questions = questions_answers.map { |row| row['question'] }
+  @answers = questions_answers.map { |row| row['answer'].to_i }
+
+  @test_name = 'test'
+  create_test_file(@test_name, @questions.join("\n"))
+
+  @one44_command = "one44-cli -t '#{@test_name}'"
+  start_test(@one44_command)
+  provide_test(@answers.join("\n"))
 end
 
 When('my Test Result will be {word}') do |result|
   expect(all_stdout).to include result
+end
+
+When('the Mark for my Answer will be {word}') do |mark|
+  expect(all_stdout).to include mark
 end
